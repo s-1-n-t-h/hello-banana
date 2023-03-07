66cd5f4e7a43db357eb6fb7dc51af84488f419f5
B V S YASWANTH <83590022+s-1-n-t-h@users.noreply.github.com>
2023-03-07 18:19:38 +0530

    changed to pytorch tensor

--- download.py
+++ download.py
@@ -7,7 +7,7 @@ from transformers import pipeline
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('summarization', model="facebook/bart-large-cnn", framework='tf',use_auth_token='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')
+    pipeline('summarization', model="facebook/bart-large-cnn", framework='pt',use_auth_token='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')
 
 if __name__ == "__main__":
     download_model()
